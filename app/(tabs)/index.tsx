777d96d50fd3004fbbd6e95ab98307481a7d077e
jacobml1449 <50343092+jacobml1449@users.noreply.github.com>
2026-01-29 18:13:27 -0600

    Assignment done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">I am Jacob Lunsford this is my Native app for the assignment.</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
